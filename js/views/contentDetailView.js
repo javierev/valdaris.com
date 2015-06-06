1ba872b16a39b4b11055964814ad587f56425435
Javier Estévez <jestevez@valdaris.com>
2015-06-07 01:00:52 +0200

    Removed some spaces

--- js/views/contentDetailView.js
+++ js/views/contentDetailView.js
@@ -2,15 +2,15 @@ window.ValdarisApp = window.ValdarisApp || {};
 
 window.ValdarisApp.ContentDetailView = Backbone.View.extend({
 	el: '#main_content',
-	template : _.template($('#template-content').html()),
+	template: _.template($('#template-content').html()),
 	initialize: function() {
 		this.render();
 	},
-	render : function() {
+	render: function() {
 		this.$el.html(this.template(this.model.toJSON()));
 		this.$el.show();
 	},
-	close : function() {
+	close: function() {
 		//remove event listeners, but there isn't any
 		this.$el.hide();
 	}
